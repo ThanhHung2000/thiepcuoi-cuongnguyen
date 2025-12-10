8c5ddfd34c4ad73da594f3dd1f727046292d28b1
PhamHung <pham14126@gmail.com>
2025-12-10 14:54:21 +0700

    add url

--- script.js
+++ script.js
@@ -260,8 +260,8 @@ function initAlbumSlider() {
 document.addEventListener('DOMContentLoaded', function() {
     // 1. LẤY CÁC PHẦN TỬ HTML
 // Thay thế URL này bằng URL Web App thực tế của bạn
-    //const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyYttJjAD7Rq3p76VY1dzqowwOeHp0uyJkrcBz3AKQsOHt-_okqxNzXyqraTlztTIp-YQ/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwiCIrWSmLXyv8jaMoSJIrK2mRqmpNsijPtKNPvLCQkSLlG27lakZBF4qYbZIyhIo8aVQ/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbywbeSALPO2PY4X67v34ADNri_k_sJ355GltiUIoJ2_ZrqpIN4V5767RyEsHnknyq1WgQ/exec';
+    //const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwiCIrWSmLXyv8jaMoSJIrK2mRqmpNsijPtKNPvLCQkSLlG27lakZBF4qYbZIyhIo8aVQ/exec';
     
     const rsvpModal = document.getElementById('rsvpModal');
     const openRsvpBtn = document.getElementById('openRsvpModal');
